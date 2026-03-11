b5b0c6560aa14bd786572bd161a989c57501a7e6
Pewepskei <philip.d.beast@gmail.com>
2026-03-11 18:46:59 +0800

    Update attribute check for permissions

--- rides/permissions.py
+++ rides/permissions.py
@@ -2,4 +2,5 @@ from rest_framework import permissions
 
 class IsAdminUserRole(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and getattr(request.user, 'role', None) == 'admin'
+        user = getattr(request, 'user', None)
+        return getattr(user, 'role', None) == 'admin'
